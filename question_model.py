4823e69e1235bcd2b718db8e45fb9c6891a82833
ABHISHEK BHARDWAJ <59165472+abhishekbhardwaj2023@users.noreply.github.com>
2021-12-30 23:14:35 +0530

    Update question_model.py

--- question_model.py
+++ question_model.py
@@ -1,7 +1,7 @@
 #Question class to make Default view of questions
 
 class Question:
-
+    # function for question
     def __init__(self, q_text, q_answer):
         self.text = q_text
         self.answer = q_answer
